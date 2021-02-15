4949c8fe0eafc179782e0daca5b9a760fabb6316
okinashunsuke <kiseven0734@gmail.com>
2021-02-15 17:56:44 +0900

    comment　function implementation

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+     redirect_to photo_path(@comment.photo)
+    else
+     @photo = @comment.photo
+      @comments = @photo.comments
+      render "photos/show"
+    end
+  end
+
+ 
+  private
+ def comment_params
+   params.require(:comment).permit(:text).merge(user_id: current_user.id, photo_id: params[:photo_id])
+ end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "photos#index"
   resources :photos do
+    resources :comments
   end
+  resources :users
 end
